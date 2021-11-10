62b6ef2f097eef7a5623bdce9b9329f96018db0b
Emmanuel Ever Telewik <emmanuelevertelewik@Emmanuels-MacBook-Air.local>
2021-11-11 01:54:44 +0800

    styling fixes

--- src/pages/products/CanopyTent/CanopyTent.js
+++ src/pages/products/CanopyTent/CanopyTent.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Features from '../../../components/page/Features'
+import FrameAnatomy from '../../../components/page/FrameAnatomy'
 import Product from '../../../components/page/Product'
 
 const CanopyTent = () => {
     return (
         <div className="canopyTent-wrapper">
             <Product />
+            <FrameAnatomy />
             <Features />
         </div>
     )
